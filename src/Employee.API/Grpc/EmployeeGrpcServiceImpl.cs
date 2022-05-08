7f06844cd52c2ddbb244f4e1119e21751516765c
MilosSlavic <62957449+MilosSlavic@users.noreply.github.com>
2022-05-08 15:13:30 +0200

    Grpc employee search

--- src/Employee.API/Grpc/EmployeeGrpcServiceImpl.cs
+++ src/Employee.API/Grpc/EmployeeGrpcServiceImpl.cs
@@ -1,22 +1,106 @@
+using Employee.API.Enums;
 using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 
 namespace Employee.API.Grpc
 {
     public class EmployeeGrpcServiceImpl : EmployeeGrpcService.EmployeeGrpcServiceBase
     {
-        public override Task<CreateReply> Create(CreateMessage request, ServerCallContext context)
+        private readonly EmployeeDbContext _dbContext;
+
+        public EmployeeGrpcServiceImpl(EmployeeDbContext dbContext)
         {
-            return base.Create(request, context);
+            _dbContext = dbContext;
         }
 
-        public override Task<GetByIdReply> GetById(GetByIdMessage request, ServerCallContext context)
+        public override async Task<CreateReply> Create(CreateMessage request, ServerCallContext context)
         {
-            return base.GetById(request, context);
+            var employee = new Employee
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                City = request.City,
+                Country = request.Country,
+                DateOfBirth = DateTime.ParseExact(request.DateOfBirth, "yyyy-MM-dd", null),
+                Email = request.Email,
+                PersonalIdentificationNumber = request.PersonalIdentificationNumber,
+                State = request.State,
+                Title = (TitleOption)request.Title
+            };
+
+            _dbContext.Employees.Add(employee);
+            await _dbContext.SaveChangesAsync();
+            return new CreateReply
+            {
+                Id = employee.Id
+            };
         }
 
-        public override Task<UpdateReply> Update(UpdateMessage request, ServerCallContext context)
+        public override async Task<GetByIdReply> GetById(GetByIdMessage request, ServerCallContext context)
         {
-            return base.Update(request, context);
+            var employee = await _dbContext.Employees.SingleOrDefaultAsync(
+                x => x.Id == request.Id);
+
+            return new GetByIdReply
+            {
+                Id = employee.Id,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                City = employee.City,
+                Country = employee.Country,
+                DateOfBirth = employee.DateOfBirth.ToString("yyyy-MM-dd"),
+                Email = employee.Email,
+                PersonalIdentificationNumber = employee.PersonalIdentificationNumber,
+                State = employee.State,
+                Title = (EmployeeTitle)employee.Title
+            };
+        }
+
+        public override async Task<UpdateReply> Update(UpdateMessage request, ServerCallContext context)
+        {
+            var employee = new Employee
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                City = request.City,
+                Country = request.Country,
+                DateOfBirth = DateTime.ParseExact(request.DateOfBirth, "yyyy-MM-dd", null),
+                Email = request.Email,
+                PersonalIdentificationNumber = request.PersonalIdentificationNumber,
+                State = request.State,
+                Title = (TitleOption)request.Title
+            };
+
+            _dbContext.Employees.Update(employee);
+            var numberOfAffectedRows = await _dbContext.SaveChangesAsync();
+            return new UpdateReply
+            {
+                Success = numberOfAffectedRows == 1
+            };
+        }
+
+        public override async Task<SearchReply> Search(SearchMessage request, ServerCallContext context)
+        {
+            var items = await _dbContext.Employees
+                .Where(x =>
+                    EF.Functions.Like(x.FirstName, $"%{request.SearchTerm}%")
+                    || EF.Functions.Like(x.LastName, $"%{request.SearchTerm}%")
+                    || EF.Functions.Like(x.Email, $"%{request.SearchTerm}%"))
+                .ToListAsync();
+
+            return new SearchReply
+            {
+                Items =
+                {
+                    items.Select(x=> new EmployeeSearchModel
+                    {
+                        Id = x.Id,
+                        FirstName = x.FirstName,
+                        LastName = x.LastName,
+                        Email = x.Email
+                    })
+                }
+            };
         }
     }
 }
--- src/EmployeeSearchBff.API/Grpc/SearchGrpcServiceImpl.cs
+++ src/EmployeeSearchBff.API/Grpc/SearchGrpcServiceImpl.cs
@@ -1,12 +1,67 @@
 using Grpc.Core;
+using static Employee.API.EmployeeGrpcService;
+using static Knowledge.API.Certificate.CertificateGrpcService;
+using static Knowledge.API.Skill.SkillGrpcService;
 
 namespace EmployeeSearchBff.API.Grpc
 {
     public class SearchGrpcServiceImpl : SearchGrpcService.SearchGrpcServiceBase
     {
-        public override Task<SearchReply> Search(SearchMessage request, ServerCallContext context)
+        private readonly EmployeeGrpcServiceClient _employeeGrpcServiceClient;
+        private readonly CertificateGrpcServiceClient _certificateGrpcServiceClient;
+        private readonly SkillGrpcServiceClient _skillGrpcServiceClient;
+
+        public SearchGrpcServiceImpl(
+            EmployeeGrpcServiceClient employeeGrpcServiceClient,
+            CertificateGrpcServiceClient certificateGrpcServiceClient,
+            SkillGrpcServiceClient skillGrpcServiceClient)
         {
-            return base.Search(request, context);
+            _employeeGrpcServiceClient = employeeGrpcServiceClient;
+            _certificateGrpcServiceClient = certificateGrpcServiceClient;
+            _skillGrpcServiceClient = skillGrpcServiceClient;
+        }
+
+        public override async Task<SearchReply> Search(SearchMessage request, ServerCallContext context)
+        {
+            var employeeSearchReply = await _employeeGrpcServiceClient.SearchAsync(new Employee.API.SearchMessage
+            {
+                SearchTerm = request.SearchTerm
+            });
+
+            var tasks = employeeSearchReply.Items
+                .Select(async x =>
+                {
+                    var hasSkillsTask = _skillGrpcServiceClient.HasSkillsAsync(new Knowledge.API.Skill.HasSkillsMessage
+                    {
+                        EmployeeId = x.Id
+                    }).ResponseAsync;
+                    var hasCertificateTask = _certificateGrpcServiceClient.HasCertificateAsync(new Knowledge.API.Certificate.HasCertificateMessage
+                    {
+                        EmployeeId = x.Id
+                    }).ResponseAsync;
+
+                    await Task.WhenAll(hasSkillsTask, hasCertificateTask);
+
+                    return new EmployeeModel
+                    {
+                        Id = x.Id,
+                        Email = x.Email,
+                        FirstName = x.FirstName,
+                        LastName = x.LastName,
+                        HasSkills = hasSkillsTask.Result.Yes,
+                        HasCertificates = hasSkillsTask.Result.Yes
+                    };
+                });
+
+            await Task.WhenAll(tasks);
+
+            return new SearchReply
+            {
+                Items =
+                {
+                    tasks.Select(x=>x.Result).ToList()
+                }
+            };
         }
     }
 }
