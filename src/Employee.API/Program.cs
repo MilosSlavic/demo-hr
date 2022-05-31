0342792ada772df2f77bb5602c0e1e6f31ac6f52
MilosSlavic <62957449+MilosSlavic@users.noreply.github.com>
2022-05-31 12:43:40 +0200

    Header propagation

--- src/Employee.API/Program.cs
+++ src/Employee.API/Program.cs
@@ -2,11 +2,22 @@ using Employee.API;
 using Employee.API.Grpc;
 using Microsoft.EntityFrameworkCore;
 using Demo.Common;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection();
+builder.Services.AddHeaderPropagation(opt =>
+{
+    opt.Headers.Add("x-request-id");
+    opt.Headers.Add("x-b3-traceid");
+    opt.Headers.Add("x-b3-spanid");
+    opt.Headers.Add("x-b3-parentspanid");
+    opt.Headers.Add("x-b3-sampled");
+    opt.Headers.Add("xx-b3-flags");
+    opt.Headers.Add("b3");
+});
 builder.Services.AddDbContext<EmployeeDbContext>((ctx, opt) =>
 {
     var configuration = ctx.GetRequiredService<IConfiguration>();
@@ -17,6 +28,7 @@ builder.Host.UseCustomLogging();
 
 var app = builder.Build();
 
+app.UseHeaderPropagation();
 app.MapGrpcService<EmployeeGrpcServiceImpl>();
 if (app.Environment.IsDevelopment())
 {
